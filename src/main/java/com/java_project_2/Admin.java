acae83232a7485e1aeff417dfd890a68a0c6c64b
Vipul.1 <Vipul.1@LDIGITBGL-L0217.godigit.corp>
2023-07-21 15:51:41 +0530

    This is 2nd project

---
+++ src/main/java/com/java_project_2/Admin.java
@@ -0,0 +1,8 @@
+package com.java_project_2;
+
+public class Admin {
+	public static void main(String[] args) {
+		System.out.println("hey future simplifier....");
+	}
+
+}
---
+++ src/main/java/com/java_project_2/package-info.java
@@ -0,0 +1,1 @@
+package com.java_project_2;
